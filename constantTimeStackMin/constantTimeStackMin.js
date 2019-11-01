ac25842ec87be99678d5534ae2515f3be6dc2491
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-11-01 09:38:51 -0700

    Completed constantTimeStackMin

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -17,24 +17,44 @@
 /**
   * Stack Class
   */
-  var Stack = function() {
+ var Stack = function() {
+  // this = Object.create()
+  this.storage = [];
+  this.minimum = [Infinity];
+  // return this
+};
 
-  // add an item to the top of the stack
-    this.push = function(value) {
-    };
-
-  // remove an item from the top of the stack
-    this.pop = function() {
-    };
-
-  // return the number of items in the stack
-    this.size = function() {
-    };
-  
-  // return the minimum value in the stack
-    this.min = function() {
-
-    };
-
+// add an item to the top of the stack
+  Stack.prototype.push = function(value) {
+    this.storage.push(value);
+    if (value < this.minimum[this.minimum.length - 1]) {
+      this.minimum.push(value);
+    }
   };
 
+// remove an item from the top of the stack
+  Stack.prototype.pop = function() {
+    if (this.storage[this.storage.length - 1] === this.minimum[this.minimum.length - 1]) {
+      this.minimum.pop();
+    }
+    return this.storage.pop();
+  };
+
+// return the number of items in the stack
+  Stack.prototype.size = function() {
+    return this.storage.length;
+  };
+
+// return the minimum value in the stack in constant time
+  Stack.prototype.min = function() {
+    return this.minimum[this.minimum.length - 1];
+  };
+
+var example = new Stack()
+example.push(4)
+example.push(3)
+console.log(example.min()) //=== 3
+example.push(3)
+example.push(2)
+example.push(2)
+console.log(example.min()) //=== 2
