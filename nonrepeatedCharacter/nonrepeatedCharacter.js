4303717206bd9e71a2b1a2a5a3275523d7f1ac5f
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-11-12 09:11:26 -0800

    Finds first nonrepeated character

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,25 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+  // object counter
+  // iterate over characters in the string
+  //    update counter for each character
+  // iterate over characters in the string
+  //    return the first string with object counter = 1
+  // return empty string
+
+  var counter = {};
+  for (var i = 0; i < string.length; i++) {
+    if (counter.hasOwnProperty(string[i])) {
+      counter[string[i]]++;
+    } else {
+      counter[string[i]] = 1;
+    }
+  }
+  for (var i = 0; i < string.length; i++) {
+    if (counter[string[i]] === 1) {
+      return string[i];
+    }
+  }
+  return '';
 };
