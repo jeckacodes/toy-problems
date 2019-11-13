3e83ffbfcc6f56bd409271515b17baf0a86a4f40
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-11-13 09:49:37 -0800

    basic find all anagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,34 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
+ var allAnagrams = function(string) {
+  var results = [];
+  var length = string.length;
+
+  var helper = (currStr, remaining) => {
+    for (var char of remaining) {
+      var newStr = currStr + char;
+      var temp = remaining.split('');
+      temp.splice(temp.indexOf(char), 1);
+      var newRemaining = temp.join('');
+      //remove char from the remaining string
+      // pass currStr and remaining as new arguments for another helper call
+      helper(newStr, newRemaining);
+//       results.push(currStr);
+//       console.log(results);
+    }
+    if (currStr.length === length) {
+      results.push(currStr);
+    }  
+  }
+
+  for (var char of string) {
+    var anagram = char;
+    var remaining = string.slice(0, string.indexOf(char)) + string.slice(string.indexOf(char) + 1);
+    helper(anagram, remaining);
+  }
+  return results;
 };
+
+// var anagrams = allAnagrams('abc');
+// console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
