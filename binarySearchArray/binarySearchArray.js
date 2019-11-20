d3110ae57dc2ecb3cf743182a7cf3e011bf54014
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-11-20 09:32:56 -0800

    Perform binary search on array

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,6 +10,25 @@
  * console.log(index); // null
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function (array, target, indexAdj) {
+    // find midpoint of array
+    // if midpoint = target, hooray you've won!
+    // else if midpoint is less than target
+    //   repeat with lower half of array
+    // else if midpoint is greater than target
+    //   repeat with upper half of array
+    // have index adjuster to keep track of location within original array
+    var indexAdj = indexAdj || 0;
+    if (array.length === 0) {
+      return null;
+    }
+    var midpoint = Math.floor(array.length/2); //midpoint is an index
+    if (array[midpoint] === target) {
+        return midpoint + indexAdj;
+    } else if (array[midpoint] < target) {
+        return binarySearch(array.slice(midpoint + 1), target, midpoint + indexAdj + 1);
+    } else if (array[midpoint] > target) {
+        return binarySearch(array.slice(0, midpoint), target, indexAdj);
+    }
 };
 
