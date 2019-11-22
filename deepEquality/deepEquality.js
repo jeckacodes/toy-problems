517dfad5cd6f7b0d35fb16ce4415a0b83e2d728d
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2019-11-22 09:28:28 -0800

    basic functionality for Deep Equality

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,32 @@
   *
   */
 var deepEquals = function(apple, orange) {
+  // first compare the keys to see if they're all the same
+    // length should be same
+    // value should be same
+  // then compare the values to see if they're all the same
+    // if the value on both is an object, run this comparison on the object
+  var keys1 = Object.keys(apple);
+  var keys2 = Object.keys(orange);
+  if (keys1.length !== keys2.length) {
+    return false;
+  } 
+  for (var key in apple) {
+    if (!orange.hasOwnProperty(key)) {
+      return false;
+    } else if (typeof apple[key] === 'object') {
+      debugger;
+      var result = deepEquals(apple[key], orange[key]);
+      if (result === false) {
+        return false;
+      }
+    } else if (apple[key] !== orange[key]) {
+      return false;
+    }
+  }
+  return true;
 };
+
+console.log(deepEquals({selfie: 3},{selfie: 3})); //true
+console.log(deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); // true
+console.log(deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); // false
