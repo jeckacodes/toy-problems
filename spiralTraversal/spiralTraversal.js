ea672a9687c1c92588ab63242131edaa839f752c
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2020-01-06 19:40:44 -0800

    spiralTraversal

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -15,6 +15,49 @@
  */
 
 var spiralTraversal = function(matrix) {
-
-  // TODO: Implement me!
+  var result = [];
+  var counter = Math.ceil(matrix.length/2);
+  function circle(matrix) {
+    var width = matrix[0].length;
+    var height = matrix.length;
+    for (let ele of matrix[0]) {
+      result.push(ele);
+    }
+    for (let i = 1; i < height; i++) {
+      result.push(matrix[i][width - 1]);
+    }
+    for (let i = width - 2; i >= 0; i--) {
+      result.push(matrix[height - 1][i]);
+    }
+    for (let i = height - 2; i > 0; i--) {
+      result.push(matrix[i][0]);
+    }
+    matrix.pop();
+    matrix.shift();
+    for (let ele of matrix) {
+      ele.pop();
+      ele.shift();
+    }
+  }
+  while (counter > 0) {
+    circle(matrix);
+    counter --;
+  }
+  return result;
 };
+
+console.log(spiralTraversal([
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]));
+console.log(spiralTraversal([
+  [1,2,3,4],
+  [5,6,7,8],
+  [9,10,11,12]
+]));
+
+console.log(spiralTraversal([
+  [1,2],
+  [3,4]
+]));
