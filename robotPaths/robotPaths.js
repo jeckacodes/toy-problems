560bb33228f20a56dd66b9996d43ef803068e181
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2020-01-08 19:35:14 -0800

    first pass robotPaths

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -20,14 +20,37 @@ var makeBoard = function(n) {
     }
   }
   board.togglePiece = function(i, j) {
+    // marks that the spot has been visited
     this[i][j] = !this[i][j];
   };
   board.hasBeenVisited = function(i, j) {
+    // checks if a spot has been visited
     return !!this[i][j];
   };
   return board;
 };
 
 var robotPaths = function(n, board, i, j) {
+  debugger;
+  var pathCount = 0;
+  const directions = [ [0, 1], [0, -1], [1, 0], [-1, 0]];
+  board.togglePiece(i,j);
+  while (!board.hasBeenVisited(n-1,n-1)) {
+    for (let direction of directions) {
+      i = i + direction[0];
+      j = j + direction[1];
+      if (!!board[i][j] && !board.hasBeenVisited(i, j)) {
+        board.togglePiece(i, j);
+        pathCount++;
+      }
+    }
+  }
+  return pathCount;
 };
 
+var board = makeBoard(1);
+console.log(robotPaths(1, board, 0, 0)); // 0
+var board = makeBoard(2);
+console.log(robotPaths(2, board, 0, 0)); // 2
+var board = makeBoard(3);
+console.log(robotPaths(3, board, 0, 0)); // 10?
