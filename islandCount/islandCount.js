ea1a910ee817474c8fd3c7decf5019261393edc1
Jessica Bainbridge-Smith <publicjbs@gmail.com>
2020-01-10 19:50:42 -0800

    islandCount first pass

--- islandCount/islandCount.js
+++ islandCount/islandCount.js
@@ -6,5 +6,56 @@
  */
 
 function countIslands(mapStr) {
-  // Your code here.
+  // for char in string
+  //   if char not visited and char = 0 (land)
+  //   mark char visited
+  //   increment island counter
+  //   findBorder()
+
+  // findBorder(idx)
+  //   var directions
+  //   for ele of directions
+  //     if string[ele] exists
+  //       mark string[ele] as visited
+  //       if string[ele] is land
+  //         findBorder(ele)  <-- this marks all land that touches the island as visited
+  var mapArray = mapStr.split('\n');
+  console.log(mapArray);
+  var visited = [];
+  var map = [];
+  for (let i = 0; i < mapArray.length; i++) {
+    map.push([]);
+    visited.push([]);
+    for (let j = 0; j < mapArray[0].length; j++) {
+      visited[i].push(false);
+      map[i].push(mapArray[i][j]);
+    }
+  }
+  debugger;
+  var islandCount = 0;
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (!visited[i][j] && map[i][j] === '0') {
+        visited[i][j] = true;
+        islandCount++;
+        findBorder(i,j);
+    }
+    }
+  }
+  function findBorder(i,j) {
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    for (let ele of directions) {
+      var newI = i + ele[0];
+      var newJ = j + ele[1];
+      if (newI > 0 && newJ > 0 && newI < map.length && newJ < map[0].length && !visited[newI][newJ]) {
+        visited[newI][newJ] = true;
+        if (map[newI][newJ] === '0') {
+          findBorder(newI, newJ);
+        }
+      }
+    }
+  }
+  return islandCount;
 }
+
+console.log(countIslands('00..\n00..\n..00')) // 2
